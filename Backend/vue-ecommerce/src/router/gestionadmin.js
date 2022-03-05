25493a8040f7c429e94084468c750992ce8c9648
unknown <daaouabdessamed@gmail.com>
2022-03-05 11:22:11 +0100

    client

---
+++ Backend/vue-ecommerce/src/router/gestionadmin.js
@@ -0,0 +1,11 @@
+import client from '../views/backOffice/client.vue'
+
+const routes = [
+  {
+    path : '/admin/client/register',
+    name : 'register',
+    component : client
+  }
+]
+
+export default routes
