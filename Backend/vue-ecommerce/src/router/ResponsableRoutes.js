1f87ed464625f6bd5c1295cb827b6763152bc0f2
el hilali <elhilali.abdelouahab@gmail.com>
2022-03-06 14:01:18 +0100

    Produit

--- Backend/vue-ecommerce/src/router/ResponsableRoutes.js
+++ Backend/vue-ecommerce/src/router/ResponsableRoutes.js
@@ -1,13 +1,20 @@
 import Login from "../views/backOffice/login.vue"
 import Home from "../views/backOffice/Home.vue"
+
 import Responsable from "../views/backOffice/responsable/responsable.vue"
 import AddResponsable from "../views/backOffice/responsable/ajouter.vue"
 import UpdateResponsable from "../views/backOffice/responsable/modifier.vue"
 import showResponsable from "../views/backOffice/responsable/afficher.vue"
+
 import Categorie from "../views/backOffice/categorie/categorie.vue"
 import AddCategorie from "../views/backOffice/categorie/ajouter.vue"
 import UpdateCategorie from "../views/backOffice/categorie/modifier.vue"
 import showCategorie from "../views/backOffice/categorie/afficher.vue"
+
+import Produit from "../views/backOffice/produit/produit.vue"
+import AddProduit from "../views/backOffice/produit/ajouter.vue"
+import UpdateProduit from "../views/backOffice/produit/modifier.vue"
+import showProduit from "../views/backOffice/produit/afficher.vue"
 
 const routes = [
     {
@@ -54,6 +61,26 @@ const routes = [
                 path : "afficher/categorie/:id",
                 name :"afficher-categorie",
                 component : showCategorie
+            },
+            {
+                path : "produit",
+                name : 'produit-page',
+                component : Produit
+            },
+            {
+                path : "ajouter/produit",
+                name : "ajouter-produit",
+                component : AddProduit
+            },
+            {
+                path : "modifier/produit/:id",
+                name : "modifier-produit",
+                component : UpdateProduit
+            },
+            {
+                path : "afficher/produit/:id",
+                name :"afficher-produit",
+                component : showProduit
             },
         ]
     
