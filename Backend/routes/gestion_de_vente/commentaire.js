1a512584be88fbbe5149b42e5dee0152036b1b93
unknown <daaouabdessamed@gmail.com>
2022-03-18 21:04:17 +0100

    merge test

--- Backend/routes/gestion_de_vente/commentaire.js
+++ Backend/routes/gestion_de_vente/commentaire.js
@@ -1,13 +1,12 @@
 const express =require('express')
 const commentaireCtrl = require("../../Controller/gestion_de_vente/commentaire");
 const router  = express.Router();
-const checkClient = require("../../midlleware/authentifiaction");
 
 
-router.post('/ajouter/:id', checkClient,commentaireCtrl.ajouter)
-router.get('/voircommentaire/:id', checkClient, commentaireCtrl.voircommentaire)
-router.put('/modifier/:id', checkClient, commentaireCtrl.modifier)
-router.delete('/supprimer/:id', checkClient, commentaireCtrl.supprimer)
+router.post('/ajouter/:id', commentaireCtrl.ajouter)
+router.get('/voircommentaire/:id', commentaireCtrl.voircommentaire)
+router.put('/modifier/:id', commentaireCtrl.modifier)
+router.delete('/supprimer/:id', commentaireCtrl.supprimer)
 
 
 module.exports = router
--- Backend/routes/livraison/panier.js
+++ Backend/routes/livraison/panier.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const panierCtrl = require("../../Controller/livraison/panier");
 const router = express.Router();
-const checkClient  = require("../../midlleware/authentifiaction");
 
-router.post("/ajouter/:id", checkClient, panierCtrl.ajouterPanier);
-router.get("/voirpanier/:id", checkClient, panierCtrl.voirpanier);
+
+router.post("/ajouter/:id",  panierCtrl.ajouterPanier);
+router.get("/voirpanier/:id",  panierCtrl.voirpanier);
 
 module.exports = router;
